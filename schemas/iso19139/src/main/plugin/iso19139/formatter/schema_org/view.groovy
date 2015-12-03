abdd4dcb1d38f1609851e2342591680cfb345043
paul van genuchten <paul.vangenuchten@geocat.net>
2015-12-03 21:22:03 +0100

    add a schema_org view to iso19139 and set as default view for html accept header

---
+++ schemas/iso19139/src/main/plugin/iso19139/formatter/schema_org/view.groovy
@@ -0,0 +1,49 @@
+handlers.start {
+    '''
+	<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Testbed data on the web</title>
+	<link href=http://getbootstrap.com/dist/css/bootstrap.min.css rel=stylesheet> 
+  </head>
+  <body>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+	'''
+	  
+	  
+}
+
+
+handlers.add select: {el -> !el.'gco:CharacterString'.text().isEmpty()}, {el ->
+    handlers.fileResult('schema_org/elem.html', [name: el.name(), text: el.text()])
+}
+
+handlers.end {
+    '''</div><script src=https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js></script>
+	<script src=../dist/js/bootstrap.min.js>
+	</body>
+	</html>'''
+}
